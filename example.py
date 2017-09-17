dfbbbe8206021564116a71f3140b2927d1329f01
¨SridharPavithrapu¨ <srpa3332@colorado.edu>
2017-09-16 21:08:11 -0600

    ¨First_commit¨

--- example.py
+++ example.py
@@ -19,8 +19,8 @@ import sys
 import os
 import datetime
 
-__author__ = "Bruce Montgomery and Bhallaji Venkatesankulathusundaram"
-__copyright__ = "Copyright (C) 2017 by Bruce Montgomery and Bhallaji Venkatesankulathusundaram"
+__author__ = "Sridhar Pavithrapu"
+__copyright__ = "Copyright (C) 2017 by Sridhar Pavithrapu"
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
@@ -45,7 +45,7 @@ class Dog(object):
 def walker(name):
     """walker: an example of a python function"""
     dog1 = Dog(name)
-    dog1.add_trick("Roll Over")
+    dog1.add_trick("Jump")
     print("Walking a new dog:")
     print("  The kind of this thing is ",dog1.kind)
     print("  This dog is ",dog1.name)
